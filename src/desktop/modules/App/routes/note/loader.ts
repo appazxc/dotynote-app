6f9a926b733c9e633dfe57c354928e18c0b790e4
Dmitry Trenkov <mirkul@gmail.com>
2023-08-02 18:39:16 +0800

    add loader mock

--- src/desktop/modules/App/routes/note/loader.ts
+++ src/desktop/modules/App/routes/note/loader.ts
@@ -1,8 +1,9 @@
 import { RouteLoader } from "shared/types/common/router";
+import { wait } from "shared/utils/wait";
 
-export const loader: RouteLoader = (params) => {
-  console.log('params', params);
+export const loader: RouteLoader = async (params) => {
+  console.log('loader params', params);
 
-
+  await wait(3000);
   
-}
+};
