ac0a6e3408a65dcc613e2aef1b3d655b92b202d3
Dmitry Trenkov <mirkul@gmail.com>
2023-11-29 12:43:42 +0800

    add device loader

--- src/shared/core/components/Main/index.tsx
+++ src/shared/core/components/Main/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { ContentLoader } from 'shared/components/ContentLoader';
 import Loadable from 'shared/components/Loadable';
 
-const MainDesktop = Loadable(() => import(/* webpackChunkName: "MainDesktop" */ 'desktop/core/Main'));
-const MainMobile = Loadable(() => import(/* webpackChunkName: "MainMobile" */ 'mobile/core/Main'));
+const MainDesktop = Loadable(
+  () => import(/* webpackChunkName: "MainDesktop" */ 'desktop/core/Main'), { fallback: <ContentLoader /> });
+const MainMobile = Loadable(
+  () => import(/* webpackChunkName: "MainMobile" */ 'mobile/core/Main'), { fallback: <ContentLoader /> });
 
 type Props = {
   isMobile: boolean,
