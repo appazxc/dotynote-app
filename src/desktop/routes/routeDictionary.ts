1f34aa095adc37b2a0b8117dbd9d202018133a56
Dmitry Trenkov <mirkul@gmail.com>
2023-11-29 12:09:58 +0800

    renames

--- src/desktop/routes/routeDictionary.ts
+++ src/desktop/routes/routeDictionary.ts
@@ -1,9 +1,9 @@
 import { routeNames } from 'shared/constants/routeNames';
 import { RouteDictionary } from 'shared/types/common/router';
 
-const Home = () => import(/* webpackChunkName: "HomePage" */ 'desktop/routes/home');
-const App = () => import(/* webpackChunkName: "AppPage" */ 'desktop/routes/app');
-const Login = () => import(/* webpackChunkName: "LoginPage" */ 'desktop/routes/login');
+const Home = () => import(/* webpackChunkName: "HomePage" */ 'desktop/routes/home1');
+const App = () => import(/* webpackChunkName: "AppPage" */ 'desktop/routes/app1');
+const Login = () => import(/* webpackChunkName: "LoginPage" */ 'desktop/routes/login1');
 
 const routeDictionary = {
   [routeNames.app]: App,
