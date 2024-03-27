8592e9dd7b785fa9788a81e22299ed0318933683
Dmitry Trenkov <mirkul@gmail.com>
2024-03-27 13:10:37 +0800

    rework trigger type

--- src/shared/components/Menu/MenuTrigger.tsx
+++ src/shared/components/Menu/MenuTrigger.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 
-interface MenuTriggerProps<T extends React.ElementType> {
-  as?: T;
-  children?: React.ReactNode;
+declare module 'react' {
+  function forwardRef<T, P = object>(
+    render: (props: P, ref: React.Ref<T>) => React.ReactNode | null
+  ): (props: P & React.RefAttributes<T>) => React.ReactNode | null;
 }
 
-const MenuTriggerInner = <T extends React.ElementType = 'div'>({
-  as,
-  ...props
-}:
-  MenuTriggerProps<T>
-  & Omit<React.ComponentPropsWithoutRef<T>, keyof MenuTriggerProps<T>>, ref) => {
-  const Component = as || 'div';
-  return <Component ref={ref} {...props} />;
-};
+type CustomComponentProps<
+  C extends keyof JSX.IntrinsicElements | React.ComponentType<any>,
+  O extends object
+> = React.ComponentPropsWithRef<
+  C extends keyof JSX.IntrinsicElements | React.ComponentType<any> ? C : never
+> &
+  O & { as?: C };
 
-export const MenuTrigger = React.forwardRef(MenuTriggerInner);
+interface CustomComponent<
+  C extends keyof JSX.IntrinsicElements | React.ComponentType<any>,
+  O extends object
+> {
+  <AsC extends keyof JSX.IntrinsicElements | React.ComponentType<any> = C>(
+    props: CustomComponentProps<AsC, O>
+  ): React.ReactElement<CustomComponentProps<AsC, O>>;
+}
+
+// @ts-ignore
+const Component: CustomComponent<'div', object> = (props, ref) => (
+  <div ref={ref} {...props} />
+);
+
+export const MenuTrigger = React.forwardRef(Component);
