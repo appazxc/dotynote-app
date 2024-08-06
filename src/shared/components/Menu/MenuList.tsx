84b965bee1903dc16b3d14ffd9d328447fea7464
Dmitry Trenkov <mirkul@gmail.com>
2024-08-06 12:23:10 +0800

    menu lightmode

--- src/shared/components/Menu/MenuList.tsx
+++ src/shared/components/Menu/MenuList.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import { Box, BoxProps, useColorModeValue } from '@chakra-ui/react';
+import { Box, BoxProps, LightMode } from '@chakra-ui/react';
 
 export const MenuList = React.forwardRef<HTMLDivElement, BoxProps>(({ children, ...props }, ref) => {
-  const bg = useColorModeValue('white', 'brand.700');
-  
   return (
-    <Box
-      ref={ref}
-      boxShadow="md"
-      borderRadius="md"
-      bg={bg}
-      display="flex"
-      flexDirection="column"
-      p="1"
-      {...props}
-      onContextMenu={(e) => {
-        e.preventDefault();
-      }}
-    >
-      {children}
-    </Box>
+    <LightMode>
+      <Box
+        ref={ref}
+        boxShadow="md"
+        borderRadius="md"
+        bg="white"
+        display="flex"
+        flexDirection="column"
+        p="1"
+        {...props}
+        onContextMenu={(e) => {
+          e.preventDefault();
+        }}
+      >
+        {children}
+      </Box>
+    </LightMode>
   );
 });
