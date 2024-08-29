8ef5dc532a4f310abbda5b7119241d974f175902
Dmitry Trenkov <mirkul@gmail.com>
2024-08-29 11:51:22 +0800

    avatar styles

--- src/shared/modules/profile/ProfileAvatar.tsx
+++ src/shared/modules/profile/ProfileAvatar.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-import { Center } from '@chakra-ui/react';
+import { Center, useColorModeValue } from '@chakra-ui/react';
 import { FaRegUser } from 'react-icons/fa6';
 
 type Props = {};
 
 export const ProfileAvatar = React.memo((props: Props) => {
+  const bg = useColorModeValue('gray.100', 'blackAlpha.300');
+
   return (
     <Center
       borderRadius="full"
       w="120px"
       h="120px"
-      bg="gray.100"
+      bg={bg}
     >
       <FaRegUser size="30" />
     </Center>
