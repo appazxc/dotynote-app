9fbe547cd67a332163c23cacf6d1e22e7e9eb352
Dmitry Trenkov <mirkul@gmail.com>
2024-09-08 16:22:26 +0800

    fix menu

--- src/shared/components/Menu/MenuDivider.tsx
+++ src/shared/components/Menu/MenuDivider.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 import { Divider } from '@chakra-ui/react';
 
+import { MenuListContext } from 'shared/components/Menu/MenuList';
+
 export const MenuDivider = () => {
+  const id = React.useId();
+  const { activeItemId } = React.useContext(MenuListContext);
+
+  if (activeItemId && activeItemId !== id) {
+    return null;
+  }
+
   return (
     <Divider my="0.5" />
   );
