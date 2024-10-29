5b0a48e1eadf47b53dc3ecf005fe06ca0e466008
Dmitry Trenkov <mirkul@gmail.com>
2024-10-29 21:00:51 +0800

    add user info to error

--- src/shared/analytics/setupSentry.ts
+++ src/shared/analytics/setupSentry.ts
@@ -1,6 +1,22 @@
 import * as Sentry from '@sentry/react';
 
 import config from 'shared/config';
+import { getStore } from 'shared/helpers/store/getStore';
+import { selectUser } from 'shared/selectors/auth/selectUser';
+
+const beforeSend = (event: Sentry.ErrorEvent) => {
+  const state = getStore().getState();
+  const user = selectUser(state);
+
+  event.user = {
+    id: user?.id,
+    email: user?.email,
+    nickname: user?.nickname,
+    username: user?.username,
+  };
+  // Добавьте любую другую информацию, которую хотите отправить
+  return event;
+};
 
 export const setupSentry = () => {
   if (!config.logging?.sentry?.enable) {
@@ -14,6 +30,7 @@ export const setupSentry = () => {
       Sentry.browserTracingIntegration(),
       Sentry.replayIntegration(),
     ],
+    beforeSend,
     replaysSessionSampleRate: 0.1, 
     replaysOnErrorSampleRate: 1.0,
   });
