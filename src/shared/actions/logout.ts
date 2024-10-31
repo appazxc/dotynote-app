819b5fefbf6563cccf5aa5149e04a854d257b7ef
Dmitry Trenkov <mirkul@gmail.com>
2024-10-31 19:06:45 +0800

    fix logout

--- src/shared/actions/logout.ts
+++ src/shared/actions/logout.ts
@@ -4,11 +4,13 @@ import { persistor } from 'shared/store';
 import { ThunkAction } from 'shared/types/store';
 
 export const logout = (): ThunkAction => async (dispatch) => {
-  persistor.purge();
+  await persistor.purge();
   
   queryClient.removeQueries();
   
   dispatch({
     type: actions.RESET_APP,
   });
+  
+  persistor.persist();
 };
