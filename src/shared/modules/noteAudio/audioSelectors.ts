93a8adf1ed1e0275d715194cda8d3d79a0698cb9
Dmitry Trenkov <mirkul@gmail.com>
2025-01-28 16:23:51 +0800

    fix: tsc

--- src/shared/modules/noteAudio/audioSelectors.ts
+++ src/shared/modules/noteAudio/audioSelectors.ts
@@ -2,18 +2,9 @@ import { createSelector } from '@reduxjs/toolkit';
 
 import { AppState } from 'shared/types/store';
 
-export const selectActiveAudio = createSelector([
-  (state: AppState) => state.entities.noteAudio,
-  (state: AppState) => state.audio.audioId,
-], (audioById, audioId) => {
-  return audioId ? audioById[audioId] : null;
-});
-
 export const selectAudioUrl: (state: AppState, audioId: string | null) => string | null = createSelector([
   (state: AppState) => state.entities.noteAudio,
   (_, audioId: string | null) => audioId,
 ], (audioById, audioId) => {
   return audioId ? audioById[audioId]?.url || null : null;
 });
-
-export const selectIsAudioPlaying = (state: AppState) => state.audio.isPlaying;
