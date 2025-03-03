13616d4761ca94a9de731596cdf0c36173bdb206
Dmitry Trenkov <mirkul@gmail.com>
2025-03-03 14:49:09 +0800

    chore: remove mock from initialize

--- src/shared/core/actions/initializeActions.ts
+++ src/shared/core/actions/initializeActions.ts
@@ -1,20 +1,5 @@
 import { setupSentry } from 'shared/analytics/setupSentry';
 
 export const initialize = async () => {
-  if (import.meta.env.MOCK === 'true') {
-    const worker = require('shared/test/mocks/browser');
-    await worker.start({
-      onUnhandledRequest(request) {
-        // Ignore any requests containing "cdn.com" in their URL.
-        if (!request.url.pathname.includes('/api/v1')) {
-          return;
-        }
-
-        // Otherwise, print an unhandled request warning.
-        // console.log('onUnhandledRequest', request);
-      },
-    });
-  }
-
   setupSentry();
 };
