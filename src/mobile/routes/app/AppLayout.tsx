6084574ab152b205df075e8e5961b00c3d163649
Dmitry Trenkov <mirkul@gmail.com>
2025-03-04 17:10:34 +0800

    chore: add cl 11

--- src/mobile/routes/app/AppLayout.tsx
+++ src/mobile/routes/app/AppLayout.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 export const AppLayout = React.memo(({ children }: Props) => {
   const { isAdvancedEditActive } = useAppSelector(state => state.app.note);
-
+console.log('111', );
   return (
     <BrowserProviders>
       <Box
