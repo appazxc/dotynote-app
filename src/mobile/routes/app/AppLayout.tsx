3b39ed276abd29ec01aea457235c69c879419a1f
Dmitry Trenkov <mirkul@gmail.com>
2025-03-04 17:16:30 +0800

    chore: cl 22

--- src/mobile/routes/app/AppLayout.tsx
+++ src/mobile/routes/app/AppLayout.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 export const AppLayout = React.memo(({ children }: Props) => {
   const { isAdvancedEditActive } = useAppSelector(state => state.app.note);
-console.log('111', );
+console.log('222', );
   return (
     <BrowserProviders>
       <Box
