c6ba2d1de71a7713e3b31b21c18a8969a4686282
Dmitry Trenkov <mirkul@gmail.com>
2025-03-04 15:47:54 +0800

    chore: remove objecturl rewrite
    blob in prod is because of sentry

--- src/shared/core/actions/initializeActions.ts
+++ src/shared/core/actions/initializeActions.ts
@@ -1,11 +1,5 @@
 import { setupSentry } from 'shared/analytics/setupSentry';
 
 export const initialize = async () => {
-  const origCreateObjectURL = URL.createObjectURL;
-  URL.createObjectURL = function(blob) {
-    console.trace('Blob created:', blob);
-    return origCreateObjectURL.call(this, blob);
-  };
-
   setupSentry();
 };
