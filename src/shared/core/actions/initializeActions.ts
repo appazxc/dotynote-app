e20ae16789199fc793022bf9eb815697eec19d09
Dmitry Trenkov <mirkul@gmail.com>
2025-03-04 14:59:59 +0800

    chore: add blob trace

--- src/shared/core/actions/initializeActions.ts
+++ src/shared/core/actions/initializeActions.ts
@@ -1,5 +1,11 @@
 import { setupSentry } from 'shared/analytics/setupSentry';
 
 export const initialize = async () => {
+  const origCreateObjectURL = URL.createObjectURL;
+  URL.createObjectURL = function(blob) {
+    console.trace('Blob created:', blob);
+    return origCreateObjectURL.call(this, blob);
+  };
+
   setupSentry();
 };
