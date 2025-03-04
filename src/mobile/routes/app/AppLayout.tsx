50e9d8939813640bda7c91fe9893f8ecc189d3ea
Dmitry Trenkov <mirkul@gmail.com>
2025-03-04 19:28:39 +0800

    13

--- src/mobile/routes/app/AppLayout.tsx
+++ src/mobile/routes/app/AppLayout.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 export const AppLayout = React.memo(({ children }: Props) => {
   const { isAdvancedEditActive } = useAppSelector(state => state.app.note);
-console.log('12', );
+console.log('13', );
   return (
     <BrowserProviders>
       <Box
