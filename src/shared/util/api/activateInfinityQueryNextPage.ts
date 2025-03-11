aa3a9cbe7704965bbfb5618c18724823a4acbae2
Dmitry Trenkov <mirkul@gmail.com>
2025-03-11 16:06:20 +0800

    feat: fix load

--- src/shared/util/api/activateInfinityQueryNextPage.ts
+++ src/shared/util/api/activateInfinityQueryNextPage.ts
@@ -6,14 +6,31 @@ export const activateInfinityQueryNextPage = (queryKey?: InfinityPostsQueryKey) 
   if (!queryKey) {
     return;
   }
-  
+
+  const queryData = queryClient.getQueryData<TQueryFnData>(queryKey);
+
+  if (!queryData) {
+    return;
+  }
+
+  const isEmpty = queryData.pageParams.length === 1 
+      && queryData.pages.length === 1 
+      && queryData.pages[0].items.length === 0;
+
+  if (isEmpty) {
+    queryClient.invalidateQueries({ queryKey: queryKey });
+    return;
+  }
+
   queryClient.setQueryData<TQueryFnData>(queryKey, (oldData) => {
     if (!oldData) {
       return oldData;
     }
-    
+    console.log('oldData', oldData);
     const filtersIndex = 2;
     const descSort = queryKey[filtersIndex]?.sort === 'desc';
+    
+    // return undefined
 
     return {
       ...oldData,
