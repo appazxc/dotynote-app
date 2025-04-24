7b0b12bda8122b470e8bb50dcf0219c49827ec11
Dmitry Trenkov <mirkul@gmail.com>
2025-04-24 20:17:58 +0800

    chore: log mode 2

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ const proxyUrl = 'http://localhost:4000/';
 export default defineConfig((params) => {
   const isProduction = params.mode === 'production';
   const buildSentry = !!process.env.SENTRY_AUTH_TOKEN;
-  console.log('isProduction', isProduction);
+  console.log('isProduction', isProduction, params.mode);
   return {
     build: {
       sourcemap: !isProduction, // Source map generation must be turned on
