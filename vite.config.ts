56ed2cd8c07ef0efa955f491757b46e09bc6a996
Dmitry Trenkov <mirkul@gmail.com>
2025-04-25 10:00:12 +0800

    chore: change manifest

--- vite.config.ts
+++ vite.config.ts
@@ -13,16 +13,36 @@ const proxyUrl = 'http://localhost:4000/';
 // https://vitejs.dev/config/
 export default defineConfig((params) => {
   const isProduction = params.mode === 'production';
-  const buildSentry = !!process.env.SENTRY_AUTH_TOKEN;
-  console.log('isProduction', isProduction, params.mode);
+
   return {
     build: {
-      sourcemap: !isProduction, // Source map generation must be turned on
+      sourcemap: true, // Source map generation must be turned on
     },
     plugins: [
       preserveDirectives(),
       react(),
       VitePWA({
+        manifest: {
+          'name': 'Dotynote',
+          'short_name': 'Dotynote',
+          'description': 'Create notes with images, videos, audio, and more.',
+          'icons': [
+            {
+              'src': '/android-chrome-192x192.png',
+              'sizes': '192x192',
+              'type': 'image/png',
+            },
+            {
+              'src': '/android-chrome-512x512.png',
+              'sizes': '512x512',
+              'type': 'image/png',
+            },
+          ],
+          'start_url': '.',
+          'display': 'standalone',
+          'theme_color': '#000000',
+          'background_color': '#ffffff',
+        },
         // registerType: 'autoUpdate',
         // workbox: {
         //   cleanupOutdatedCaches: true,
@@ -43,11 +63,15 @@ export default defineConfig((params) => {
           export default {};
         `,
       }) : null,
-      ...buildSentry ? [sentryVitePlugin({
+      sentryVitePlugin({
         authToken: process.env.SENTRY_AUTH_TOKEN,
         org: 'dotify',
         project: 'frontend',
-      })] : [],
+        sourcemaps: {
+          filesToDeleteAfterUpload: 'dist/**/*.map',
+        },
+        disable: !process.env.SENTRY_AUTH_TOKEN,
+      }),
     ],
     resolve: {
       alias: {
