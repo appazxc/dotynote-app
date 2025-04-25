9ec20cbc61ad4564ce55f8c712cb3ba1a07c0d9c
Dmitry Trenkov <mirkul@gmail.com>
2025-04-25 10:48:48 +0800

    chore: upgrade tanstack-router
    fix 300ms delay

--- src/desktop/modules/space/tabRoutes/root/index.tsx
+++ src/desktop/modules/space/tabRoutes/root/index.tsx
@@ -2,7 +2,7 @@ import {
   Outlet,
   createRootRoute,
 } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import React from 'react';
 
 import config from 'shared/config';
