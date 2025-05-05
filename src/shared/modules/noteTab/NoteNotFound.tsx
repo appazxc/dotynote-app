ccb4558effbe1cb7afeba3c9b036d06f06456d20
Dmitry Trenkov <mirkul@gmail.com>
2025-05-05 15:26:48 +0800

    feat: note 404

--- src/shared/modules/noteTab/NoteNotFound.tsx
+++ src/shared/modules/noteTab/NoteNotFound.tsx
@@ -1,31 +1,16 @@
-import { Box, Center, Heading, Text } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+import { TbError404 } from 'react-icons/tb';
+
+import { EmptyState } from 'shared/components/ui/empty-state';
 
 export const NoteNotFound = () => {
   return (
     <Center h="full">
-      <Box
-        textAlign="center"
-        py={10}
-        px={6}
-      >
-        <Heading
-          display="inline-block"
-          as="h2"
-          size="2xl"
-        >
-          404
-        </Heading>
-        <Text
-          fontSize="18px"
-          mt="3"
-          mb="2"
-        >
-          Note Not Found
-        </Text>
-        <Text color={'gray.500'} mb={6}>
-          The note you{"'"}re looking for does not seem to exist
-        </Text>
-      </Box>
+      <EmptyState
+        icon={<TbError404 />}
+        title="Note Not Available"
+        description="The note you are looking for cannot be found"
+      />
     </Center>
   );
 };
